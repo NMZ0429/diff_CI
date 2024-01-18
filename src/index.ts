b49945c47fa0d5e308b6045c271533b485814dd8
NMZ0429 <gen0429@icloud.com>
2024-01-18 16:51:26 +0900

    migrate to ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-const core = require("@actions/core");
-const { Octokit } = require("@octokit/rest");
+import { getInput, setFailed } from "@actions/core";
+import { Octokit } from "@octokit/rest";
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 const pull_number = process.env.GITHUB_REF.split("/").slice(-1)[0];
-const token = core.getInput("github-token");
+const token = getInput("github-token");
 const octokit = new Octokit({ auth: `token ${token}` });
 octokit.pulls
 	.get({
 		owner: owner,
 		repo: repo,
-		pull_number: pull_number,
+		pull_number: +pull_number,
 		mediaType: {
 			format: "diff",
 		},
 	})
 	.then(({ data }) => {
-		const lines = data.split("\n");
+		const lines = data.toString().split("\n");
 		const count = lines.filter(
 			(line) => line.startsWith("+ ") || line.startsWith("- ")
 		).length;
@@ -23,5 +23,5 @@ octokit.pulls
 		console.log(count);
 	})
 	.catch((err) => {
-		core.setFailed(err.message);
+		setFailed(err.message);
 	});
