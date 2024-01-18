c66390b3b314e6bb6931692aea82d09be039b193
NMZ0429 <gen0429@icloud.com>
2024-01-18 18:31:07 +0900

    Add error handling for missing environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { getInput, setFailed } from "@actions/core";
 import { Octokit } from "@octokit/rest";
 
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const { GITHUB_REPOSITORY, GITHUB_SHA } = process.env;
+if (!GITHUB_REPOSITORY) {
+	setFailed("GITHUB_REPOSITORY is not defined");
+}
+if (!GITHUB_SHA) {
+	setFailed("GITHUB_SHA is not defined");
+}
+const [owner, repo] = GITHUB_REPOSITORY.split("/");
 const pull_number = process.env.GITHUB_REF.split("/").slice(-1)[0];
 const token = getInput("github-token");
 const octokit = new Octokit({ auth: `token ${token}` });
